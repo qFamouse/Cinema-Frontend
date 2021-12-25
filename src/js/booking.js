3135d5a19fa4964e3e3fd449e4f26e8428011a81
qFamouse <famouse_ru@mail.ru>
2021-12-25 21:15:22 +0300

    Start booking

---
+++ src/js/booking.js
@@ -0,0 +1,17 @@
+// const booking = document.getElementById('booking');
+//
+// screen = document.createElement('div');
+// screen.classList.add('booking__screen')
+//
+// seats = document.createElement('div')
+// seats.classList.add('booking__rows')
+//
+// booking.appendChild(screen);
+// booking.appendChild(seats);
+//
+// for (let i = 0; i < 5; i++) {
+//     seats.appendChild(document.createElement('div'))
+// }
+//
+//
+//
